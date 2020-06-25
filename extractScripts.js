3e83978d6019c685e48b92f039a970fea26fa5b5
David Morejon <dmorejon@google.com>
2020-06-25 15:11:42 +0000

    Extracts the JS  within <script> tags in HTML test.
    The scripts are placed in a temporary directory, and each one is its own
    .js file.

---
+++ extractScripts.js
@@ -0,0 +1,67 @@
+const fs          = require("fs");
+const htmlparser2 = require("htmlparser2");
+
+// TODO (dmorejon): add comment here describing function
+// espeically the temporary files naming, location, and structure
+function extractScriptsFromHTML(filePath) {
+    let scriptCount = 0;
+    let inScript = false;
+    let currentFile = '';
+    // Creates a directory to hold all of the JS scripts in this HTML test file, 
+    // {recursive: true} will ensure an error is not thrown if the directory exists.
+    fs.mkdir("./tempJsScripts", {recursive: true}, function (err) {
+        if (err) { throw err; }
+    });
+    const parser = new htmlparser2.Parser(
+        {
+            onopentag(name, attribs) {
+                if (name === "script") {
+                    inScript = true;
+                    console.log("Encountered script open");
+                    console.log("attribs:", attribs);
+                    // TODO: adjust attribute here?
+                    currentFile = "./tempJsScripts/script" + scriptCount + "_" + fileNameFromPath(filePath) + ".js";
+                    // Creates a file, overwriting any existing file, with empty contents.
+                    fs.writeFile(currentFile, '', function (err) {
+                        if (err) { throw err; }
+                    });
+                }
+            },
+            ontext(text) {
+                if (inScript) {
+                    fs.appendFile(currentFile, text, function (err) {
+                        if (err) { throw err; }
+                    });
+                }
+            },
+            onclosetag(tagname) {
+                if (tagname === "script") {
+                    inScript = false;
+                    scriptCount++;
+                    console.log("encountered script end.");
+                }
+            }
+        }
+    );
+
+    fs.readFile(filePath, "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        parser.write(data);
+        parser.end();
+        
+    });
+}
+
+// Given a string path to a file,
+// returns the name of the file without the file extension.
+function fileNameFromPath(filePath) {
+    directorySplit = filePath.split("/")
+    fileNameWithExtension = directorySplit[directorySplit.length - 1]
+    // Return the name without the extension
+    return fileNameWithExtension.split(".")[0] 
+}
+
+filePath = "./example_legacy_tests/file-list-test.html";
+extractScriptsFromHTML(filePath);
