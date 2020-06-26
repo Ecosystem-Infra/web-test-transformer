4d4c00fb34aca091d5cac5db4df0ca73ebc29d7e
David Morejon <dmorejon@google.com>
2020-06-26 21:52:30 +0000

    BROKEN: debugging temp directory empty file read

--- extractScripts.js
+++ extractScripts.js
@@ -1,32 +1,33 @@
 "use strict";
 const fs          = require("fs");
 const htmlparser2 = require("htmlparser2");
+const tmp         = require("tmp");
 
 const SCRIPT = "script";
 // IMPORTANT: if file naming convention changes, update corresponding strings in testExtractScripts.js
-const TEMP_DIR = "./tempJsScripts";
+const TEMP_DIR = "tempJsScripts";
 
 // extractScriptsFromHTML takes a string filePath: a path to the HTML test
 // file that you want to extract the javascript from within <script> tags.
 // It creates a directory "tempJsScripts" in the same directory as this file,
 // then puts the .js scripts extracted in the new dir, named/numbered script{0}_{test filename}.js 
-function extractScriptsFromHTML(filePath) {
+function extractScriptsFromHTML(filePath, tempDir=null) {
     let scriptCount = 0;
     let inScript = false;
     let currentFile = '';
     let files = [];
-    // Creates a directory to hold all of the JS scripts in this HTML test file, 
-    // {recursive: true} will ensure an error is not thrown if the directory exists.
-    fs.mkdir(TEMP_DIR, {recursive: true}, function (err) {
-        if (err) { throw err; }
-    });
+    // Creates a temporary directory to hold all of the JS scripts in this HTML test file. 
+    if (!tempDir) {
+        tempDir = tmp.dirSync({prefix: TEMP_DIR, tmpdir: "."})
+    }
+
     const parser = new htmlparser2.Parser(
         {
             onopentag(tagname, attribs) {
                 if (tagname === SCRIPT) {
                     inScript = true;
                     let newFileName = "/script" + scriptCount + "_" + fileNameFromPath(filePath) + ".js";
-                    currentFile = TEMP_DIR + newFileName; 
+                    currentFile = tempDir.name + newFileName; 
                     files.push(currentFile);
                     // Creates a file, overwriting any existing file, with empty contents.
                     fs.writeFile(currentFile, '', function (err) {
@@ -53,7 +54,6 @@ function extractScriptsFromHTML(filePath) {
     );
 
     let data = fs.readFileSync(filePath, "utf8");
-
     parser.write(data);
     parser.end();
     return files;
@@ -68,8 +68,8 @@ function fileNameFromPath(filePath) {
     return fileNameWithExtension.split(".")[0] 
 }
 
-let filePath = "./example_legacy_tests/file-list-test.html";
-let filesWritten = extractScriptsFromHTML(filePath);
-console.log("Wrote ", filesWritten);
+//let filePath = "./example_legacy_tests/file-list-test.html";
+//let filesWritten = extractScriptsFromHTML(filePath);
+//console.log("Wrote ", filesWritten);
 
 module.exports = extractScriptsFromHTML;
--- test/testExtractScripts.js
+++ test/testExtractScripts.js
@@ -1,23 +1,54 @@
 let assert = require("assert");
+let fs     = require("fs");
+let tmp    = require("tmp");
 let extractScriptsFromHTML = require("../extractScripts.js");
+const { Parser } = require("htmlparser2");
 
 // IMPORTANT: if file naming convention changes in file, change this function. 
-function expectedFiles(fileName, numberOfScriptTags) {
-    const base = "./tempJsScripts/script";
+function expectedFiles(fileName, dirName, numberOfScriptTags) {
+    const referenceBase = "./test/referenceFiles/ref_";
     let files = [];
+    let referenceFiles = [];
     for (let i = 0; i < numberOfScriptTags; i++) {
-        files.push(base + i + "_" + fileName + ".js");
+        let newFileName = "script" + i + "_" + fileName + ".js";
+        files.push(dirName + "/" + newFileName);
+        referenceFiles.push(referenceBase + newFileName);
     }
-    return files;
+    return {fileList: files, references: referenceFiles}
 }
 
 describe("#extractScriptsFromHTML()", function() {
+    let tempDir = null;
+    before(function() {
+        console.log("running before")
+        tempDir = tmp.dirSync({name: "testing1", prefix: "testTemp", unsafeCleanup: false, tmpdir: "."});
+        console.log(tempDir.name)
+    });
+
+    after(function() {
+        console.log("running after")
+        //tempDir.removeCallback();
+    });
 
     context("2 sets of <script> tags- file-list-test.html", function() {
-        it('should return correct list of files files', function() {
-            let actual = extractScriptsFromHTML("./example_legacy_tests/file-list-test.html");
-            let expected = expectedFiles("file-list-test", 2);
-            assert.deepEqual(actual, expected);
+        const testPath = "./example_legacy_tests/file-list-test.html";
+
+        //it("should return correct list of files", function() {
+            //let actualFileList = extractScriptsFromHTML(testPath, tempDir);
+            //let expected = expectedFiles("file-list-test", "./testing1", 2);
+            //assert.deepEqual(actualFileList, expected.fileList);
+        //});
+
+        it("should write files with the correct javascript inside", function () {
+            let actualFileList = extractScriptsFromHTML(testPath, tempDir);
+            let expected = expectedFiles("file-list-test", "./testing1", 2);
+            for (let i = 0; i < actualFileList.length; i++) {
+                console.log("reading from : ", actualFileList[i])
+                let actualContents = fs.readFileSync(actualFileList[i], "utf-8");
+                console.log("actual: " + actualContents)
+                let expectedContents = fs.readFileSync(expected.references[i], "utf-8");
+                assert.equal(actualContents, expectedContents);
+            }
         });
     });
 });
