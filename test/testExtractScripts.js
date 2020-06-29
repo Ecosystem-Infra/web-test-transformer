22620a79233775185289b75f5eb00adaf4312b58
David Morejon <dmorejon@google.com>
2020-06-29 20:58:46 +0000

    ESLint setup and linter fixes. Moved files to src/ and test/

--- test/testExtractScripts.js
+++ test/testExtractScripts.js
@@ -1,51 +1,51 @@
-let assert = require("assert");
-let fs     = require("fs");
-let tmp    = require("tmp");
+const assert = require('assert');
+const fs = require('fs');
+const tmp = require('tmp');
 
-let extractScriptsFromHTML = require("../extractScripts.js");
+const extractScriptsFromHTML = require('../src/extractScripts.js');
 
-// IMPORTANT: if file naming convention changes in file, change this function. 
+// IMPORTANT: if file naming convention changes in file, change this function.
 function expectedResults(fileName, dirName, numberOfScriptTags) {
-    const referenceBase = "./test/referenceFiles/ref_";
-    let files = [];
-    let referenceFiles = [];
-    for (let i = 0; i < numberOfScriptTags; i++) {
-        let newFileName = "script" + i + "_" + fileName + ".js";
-        files.push(dirName + "/" + newFileName);
-        referenceFiles.push(referenceBase + newFileName);
-    }
-    return {fileList: files, references: referenceFiles}
+  const referenceBase = './test/referenceFiles/ref_';
+  const files = [];
+  const referenceFiles = [];
+  for (let i = 0; i < numberOfScriptTags; i++) {
+    const newFileName = 'script' + i + '_' + fileName + '.js';
+    files.push(dirName + '/' + newFileName);
+    referenceFiles.push(referenceBase + newFileName);
+  }
+  return {fileList: files, references: referenceFiles};
 }
 
-describe("#extractScriptsFromHTML()", function() {
-    let tempDir = null;
-    before(function() {
-        tempDir = tmp.dirSync({prefix: "testTemp", unsafeCleanup: true, tmpdir: "."});
+describe('#extractScriptsFromHTML()', function() {
+  let tempDir = null;
+  before(function() {
+    tempDir = tmp.dirSync({prefix: 'testTmp', unsafeCleanup: true, tmpdir: '.'});
+  });
+
+  after(function() {
+    tempDir.removeCallback();
+  });
+
+  context('2 sets of <script> tags- file-list-test.html', function() {
+    const testPath = './example_legacy_tests/file-list-test.html';
+    const testFileName = 'file-list-test';
+    const testScriptCount = 2;
+
+    it('should return correct list of files', function() {
+      const actualFileList = extractScriptsFromHTML(testPath, tempDir);
+      const expected = expectedResults(testFileName, tempDir.name, testScriptCount);
+      assert.deepEqual(actualFileList, expected.fileList);
     });
 
-    after(function() {
-        tempDir.removeCallback();
+    it('should write files with the correct javascript inside', function() {
+      const actualFileList = extractScriptsFromHTML(testPath, tempDir);
+      const expected = expectedResults(testFileName, tempDir.name, testScriptCount);
+      for (let i = 0; i < actualFileList.length; i++) {
+        const actualContents = fs.readFileSync(actualFileList[i], 'utf-8');
+        const expectedContents = fs.readFileSync(expected.references[i], 'utf-8');
+        assert.equal(actualContents, expectedContents);
+      }
     });
-
-    context("2 sets of <script> tags- file-list-test.html", function() {
-        const testPath = "./example_legacy_tests/file-list-test.html";
-        const testFileName = "file-list-test";
-        const testScriptCount = 2;
-
-        it("should return correct list of files", function() {
-            let actualFileList = extractScriptsFromHTML(testPath, tempDir);
-            let expected = expectedResults(testFileName, tempDir.name, testScriptCount);
-            assert.deepEqual(actualFileList, expected.fileList);
-        });
-
-        it("should write files with the correct javascript inside", function () {
-            let actualFileList = extractScriptsFromHTML(testPath, tempDir);
-            let expected = expectedResults(testFileName, tempDir.name, testScriptCount);
-            for (let i = 0; i < actualFileList.length; i++) {
-                let actualContents = fs.readFileSync(actualFileList[i], "utf-8");
-                let expectedContents = fs.readFileSync(expected.references[i], "utf-8");
-                assert.equal(actualContents, expectedContents);
-            }
-        });
-    });
+  });
 });
