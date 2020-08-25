64017009df73d39422d0573c45080a57aba94102
David Morejon <dmorejon@google.com>
2020-08-25 17:39:06 +0000

    Implement shouldBeType transformation

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const QUIET_FLAG = 'quiet';
 const TARGET_BUILD_FLAG = 'target_build';
 const VERIFY_FLAG = 'verify';
 
+// If flags are added, deleted, or modified, UPDATE README.
+
 // Specify exactly one of --file or --dir.
 flags.defineString(FILE_FLAG, null, 'Path to test file to transform');
 flags.defineString(DIR_FLAG, null, 'Path to dir of test files to transform');
@@ -60,8 +62,10 @@ async function main() {
   // results.
 
   const fileNames = fs.readdirSync(dir);
+  const completedTransformations = [];
   const skippedTransformations = [];
   const failedTransformations = [];
+  const successfulVerifications = [];
   const failedVerifications = [];
   fileNames.forEach((fileName) => {
     if (fileName.split('.').pop() !== 'html') {
@@ -70,7 +74,13 @@ async function main() {
     const filePath = dir + '/' + fileName;
     const result = transformFile(filePath, outputDir);
     if (result === transformResult.SUCCESS) {
-      if (verify && !verifyTransformation(filePath, targetBuild)) {
+      completedTransformations.push(filePath);
+      if (!verify) {
+        return;
+      }
+      if (verifyTransformation(filePath, targetBuild)) {
+        successfulVerifications.push(filePath);
+      } else {
         failedVerifications.push(filePath);
       }
     } else if (result === transformResult.FAIL) {
@@ -79,9 +89,19 @@ async function main() {
       skippedTransformations.push(filePath);
     }
   });
-  log('Skipped Transformations:', skippedTransformations);
-  log('Failed Transformations:', failedTransformations);
-  log('Failed Verifications:', failedVerifications);
+  log('Transformation Results:');
+  log('Completed Transformations:');
+  console.log(completedTransformations.join('\n'));
+  log('Skipped Transformations:');
+  console.log(skippedTransformations.join('\n'));
+  log('Failed Transformations:');
+  console.log(failedTransformations.join('\n'));
+
+  log('\nVerification Results:');
+  log('Succesful Verifications:');
+  console.log(successfulVerifications.join('\n'));
+  log('Failed Verifications:');
+  console.log(failedVerifications.join('\n'));
 }
 
 main().catch((reason) => {
